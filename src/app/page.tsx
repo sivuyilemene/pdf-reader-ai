c6efe46e8d008c1376f39eff09b17b238cde85c4
simene022 <simene022@student.wethinkcode.co.za>
2023-09-19 20:36:34 +0200

    add(home page): added components to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,47 @@
 import { Button } from '@/components/ui/button'
-import { UserButton } from '@clerk/nextjs'
+import { UserButton, auth } from '@clerk/nextjs'
 import Image from 'next/image'
+import Link from 'next/link';
+import {LogIn} from 'lucide-react'
 
 export default async function Home() {
-  return(
+  // checks if user is signed in 
+  const { userId } = await auth();
+  const isAuth = Boolean(userId);
+  return (
     // give background a gradient
-    <div className="w-screen min-h-screen bg-gradient-to-r from-orange-600 to-orange-500">
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div className="flex items-center">
-          <h1 className="mr-3 text-5x1 font-semibold">Chat with my PDF</h1>
-              <UserButton afterSignOutUrl="/"/>
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chat with my PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+
+          <div className="flex mt-2">
+            {isAuth && <Button>Go to chats</Button>}
+
+          </div>
+
+          <p className="max-w-xl mt-1 text-lg text-slate-600">
+            Join millions of students reaserchers and
+            profesionanall to instantly understand research
+          </p>
+
+          <div className="w-full mt-4">
+            {isAuth ? (<h1>upload</h1>) : (
+              <Link href="/sign-in"> 
+                <Button>
+                  Login to get started 
+                  <LogIn className="w-4 h-4 ml-2"></LogIn>
+                </Button>
+              </Link>
+            )}
+          </div>
+
         </div>
       </div>
     </div>
   )
-  
-  }
+
+}
